e10ae875da5a1cc8d34ff598de965ab59575df94
ezragordy <ezragordy@icloud.com>
2026-02-22 11:52:01 +0300

    Clear branch

--- app/log/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-import { router } from 'expo-router';
-
-export default function LogIndex() {
-  useEffect(() => {
-    router.replace('/log/category');
-  }, []);
-  return null;
-}
